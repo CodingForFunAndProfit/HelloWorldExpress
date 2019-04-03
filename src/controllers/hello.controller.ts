d9d7132e89ab1ecd2ca99ce8fbfbd679076dca64
CodingForFunAndProfit <mail@florianstolz.net>
2019-04-03 20:28:12 +0200

    Changes in branchtest1

--- src/controllers/hello.controller.ts
+++ src/controllers/hello.controller.ts
@@ -3,7 +3,7 @@ import { Request, Response, Router } from 'express';
 const router: Router = Router();
 
 router.get('/', (req: Request, res: Response) => {
-    res.send('Hello, World!!');
+    res.send('Hello, World!');
 });
 
 router.get('/:name', (req: Request, res: Response) => {
