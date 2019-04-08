cfe27ee7be7ae02a042ffe47dd07c0950270d24e
CodingForFunAndProfit <mail@florianstolz.net>
2019-04-08 23:05:40 +0200

    changed sequelize/config.js

--- sequelize/config.js
+++ sequelize/config.js
@@ -20,10 +20,7 @@ module.exports = {
         dialect: 'postgres'
     },
     production: {
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOSTNAME,
+        use_env_variable: 'DATABASE_URL',
         port: 5433,
         dialect: 'postgres',
         ssl: true,
