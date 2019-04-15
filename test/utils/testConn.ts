40521bd52f1db1dc0cab463254cbd4afeed21562
CodingForFunAndProfit <mail@florianstolz.net>
2019-04-15 09:56:55 +0200

    travis ci test/postgres adjustments

--- test/utils/testConn.ts
+++ test/utils/testConn.ts
@@ -1,16 +1,27 @@
 import { createConnection } from 'typeorm';
 
 export const testConn = (drop: boolean = false) => {
-    return createConnection({
-        name: 'default',
-        type: 'postgres',
-        host: 'localhost',
-        port: 5433,
-        username: 'username',
-        password: 'password',
-        database: 'database-test',
-        synchronize: drop,
-        dropSchema: drop,
-        entities: [__dirname + '/../../src/db/entity/*.*']
-    });
+    if (process.env.DATABASE_URL) {
+        return createConnection({
+            name: 'default',
+            type: 'postgres',
+            url: process.env.DATABASE_URL,
+            synchronize: drop,
+            dropSchema: drop,
+            entities: [__dirname + '/../../src/db/entity/*.*']
+        });
+    } else {
+        return createConnection({
+            name: 'default',
+            type: 'postgres',
+            host: 'localhost',
+            port: 5433,
+            username: 'username',
+            password: 'password',
+            database: 'database-test',
+            synchronize: drop,
+            dropSchema: drop,
+            entities: [__dirname + '/../../src/db/entity/*.*']
+        });
+    }
 };
