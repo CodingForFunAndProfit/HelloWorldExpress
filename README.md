575a9801640315629cc69a0bcd8da299b5a0a255
CodingForFunAndProfit <mail@florianstolz.net>
2020-03-14 23:21:30 +0100

    Packages update. Bug in there, see Readme

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # HelloWorldExpress
 
 This Repository is a testing project to get things working on heroku & travis
+
+## TODO
+
+Bug with wrong redis credentials
--- src/app.ts
+++ src/app.ts
@@ -7,8 +7,8 @@ import { HelloController } from './controllers';
 import { ApolloServer } from 'apollo-server-express';
 import { createSchema } from './db/createSchema';
 import queryComplexity, {
-    fieldConfigEstimator,
-    simpleEstimator
+    simpleEstimator,
+    fieldExtensionsEstimator,
 } from 'graphql-query-complexity';
 import connectRedis from 'connect-redis';
 import { redis } from './redis';
@@ -45,15 +45,15 @@ const main = async () => {
                 },
                 estimators: [
                     // Using fieldConfigEstimator is mandatory to make it work with type-graphql
-                    fieldConfigEstimator(),
+                    fieldExtensionsEstimator(),
                     // This will assign each field a complexity of 1 if no other estimator
                     // returned a value. We can define the default value for fields not explicitly annotated
                     simpleEstimator({
-                        defaultComplexity: 1
-                    })
-                ]
-            }) as any
-        ]
+                        defaultComplexity: 1,
+                    }),
+                ],
+            }) as any,
+        ],
     });
 
     // const clientOrigin = origin: 'http://herokuurl';
@@ -61,7 +61,7 @@ const main = async () => {
     app.use(
         cors({
             credentials: true,
-            origin: clientOrigin
+            origin: clientOrigin,
         })
     );
 
@@ -70,7 +70,7 @@ const main = async () => {
     app.use(
         session({
             store: new RedisStore({
-                client: redis as any
+                client: redis as any,
             }),
             name: 'qid',
             secret: process.env.SESSION_SECRET,
@@ -79,8 +79,8 @@ const main = async () => {
             cookie: {
                 httpOnly: true,
                 secure: process.env.NODE_ENV === 'production',
-                maxAge: 1000 * 60 * 60 * 24 * 7 * 365 // 7 years
-            }
+                maxAge: 1000 * 60 * 60 * 24 * 7 * 365, // 7 years
+            },
         } as any)
     );
 
